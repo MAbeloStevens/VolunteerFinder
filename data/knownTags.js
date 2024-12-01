27590c4953f7a1f1e96aeae745e4f3913626c6b7
KP0823 <kushparmar0823@gmail.com>
2024-11-30 20:48:06 -0500

    Added Function headers to these files, also indicated which ones were completed.

--- data/knownTags.js
+++ data/knownTags.js
@@ -1,2 +1,20 @@
 import {knownTags} from "../config/mongoCollections.js";
 import validation from '../helpers/validation.js';
+
+const knownTagsFunctions  ={
+    async getKnownTags (){
+        //Get all known tags function
+        console.log("Is implemented, just needs more test cases")
+    },
+
+    async addToKnownTags (listOfTags) {
+        //Given a list of tags, add them to the knownTags array
+        console.log("Is implemented, just needs more test cases")
+    },
+
+    async deleteKnownTag (tag) {
+        //Given string, delete that string from tags list (just in case)
+        console.log("Is implemented, just needs more test cases")
+    }
+}
+export default knownTagsFunctions;
--- data/organizations.js
+++ data/organizations.js
@@ -1,2 +1,44 @@
 import {organizations} from "../config/mongoCollections.js";
 import validation from '../helpers/validation.js';
+import knownTagsFunctions from './knownTags.js';
+const organizationFunctions ={
+    async getOrganizationPageData(o_id){
+        //Given o_id, return data required for organization page elements
+        console.log("Implement Me")
+    },
+
+    async getOrganizationsInterest(o_idList){
+        //Given list of o_ids, return list of projections containing o_id, name, interestedAccounts
+        console.log("Implement Me")
+    },
+    
+    async getOrganizationsTags(o_idList) {
+        //Given list of o_ids, return list of projections containing, o_id, name, tags
+        console.log("Implement Me")
+    },
+
+    async getOrganizationsWithTags(tags){
+        //Given list of tags, return a list of organization ids that contain ANY of the tags
+        console.log("Implement Me")
+    },
+
+    async getOrganizationsWithAllTags(tags){
+        //Given list of tags, return a list of organization ids that contain ALL of the tags
+        console.log("Implement Me")
+    },
+
+    async createOrganziation(newOrganization){
+        //Create organization function with required information and optional information
+        console.log("Mostly Implemented, check branch")
+    },
+    async updateOrganization(o_id){
+        //update Organization, i am assuming you need the o_id 
+        console.log("Implement Me")
+    },
+
+    async deleteOrganization(o_id){
+        //delete Organization given o_id
+        console.log("Implement Me")
+    }
+}
+export default organizationFunctions;
